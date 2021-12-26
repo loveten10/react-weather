0f4b7d65412d7d25aa61d6f6be5aede555af2562
ryan <a60510084yahoo.com.tw@gmail.com>
2021-12-26 16:30:26 +0800

    create pages home

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Home from './pages/home';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <Home></Home>
   </React.StrictMode>,
   document.getElementById('root')
 );
