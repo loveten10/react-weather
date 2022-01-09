fca745a86d8837688e2f269816dcad87fff4b9a4
ryan <a60510084yahoo.com.tw@gmail.com>
2022-01-09 17:05:54 +0800

    add redux and add sideMenu component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Home from './pages/home';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import SideMenu from "./components/side-menu";
+import "./index.model.scss";
+import Home from "./pages/home";
+import reportWebVitals from "./reportWebVitals";
+import { Drawer } from "@mui/material";
 ReactDOM.render(
-  <React.StrictMode>
-    <Home></Home>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <div className="body">
+            <SideMenu></SideMenu>
+            <Home></Home>
+        </div>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
